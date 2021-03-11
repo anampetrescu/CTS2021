f617dc075943361af2a5b2f37dac00478c00258f
anampetrescu <anamaria.petrescu03@gmail.com>
2021-03-11 17:19:50 +0200

    Second commit

--- CTS_Seminar2/src/ro/ase/cts/program/Program.java
+++ CTS_Seminar2/src/ro/ase/cts/program/Program.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import ro.ase.cts.clase.Angajat;
 import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.Proiect;
+import ro.ase.cts.clase.Student;
 import ro.ase.cts.clase.readers.AngajatReader;
 import ro.ase.cts.clase.readers.AplicantReader;
 
@@ -14,12 +16,18 @@ public class Program {
 		return reader.readAplicanti();
 	}
 	public static void main(String[] args) {
+		System.out.println(Angajat.getSumaFinantare());
+		System.out.println(Student.getSumaFinantare());
+		
+		Proiect proiectInitial=new Proiect(80);
+		
 		List<Aplicant> listaAngajati;
 		try {
 			listaAngajati = citesteAplicanti(new AngajatReader("angajati.txt"));
 			for(Aplicant angajat:listaAngajati) {
 				System.out.println(angajat.toString());
 				System.out.println("Angajatul are finantarea: "+angajat.getFinantare());
+				angajat.afisareStatutPeProiect(proiectInitial);
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
