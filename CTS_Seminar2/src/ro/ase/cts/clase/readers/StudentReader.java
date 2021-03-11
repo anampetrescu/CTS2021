85b66ec18cfc9567d2dd83ee533344fd8c03596f
anampetrescu <anamaria.petrescu03@gmail.com>
2021-03-11 16:28:58 +0200

    Commit

--- CTS_Seminar2/src/ro/ase/cts/clase/readers/StudentReader.java
+++ CTS_Seminar2/src/ro/ase/cts/clase/readers/StudentReader.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Scanner;
 
 import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.Elev;
 import ro.ase.cts.clase.Student;
 
 public class StudentReader extends AplicantReader {
@@ -22,18 +23,13 @@ public class StudentReader extends AplicantReader {
 		List<Aplicant> studenti = new ArrayList<Aplicant>();
 
 		while (input.hasNext()) {
-			String nume = input.next();
-			String prenume = (input.next()).toString();
-			int varsta = Integer.valueOf(input.nextInt());
-			int punctaj = Integer.valueOf(input.nextInt());
-			int nr = Integer.valueOf(input.nextInt());
-			String[] vect = new String[5];
-			for (int i = 0; i < nr; i++)
-				vect[i] = input.next();
+			Student student=new Student();
+			super.readAplicant(input, student);
 			int an_studii = input.nextInt();
 			String facultate = (input.next()).toString();
-			Student s = new Student(nume, prenume, varsta, punctaj, nr, vect, facultate, an_studii);
-			studenti.add(s);
+			student.setAn_studii(an_studii);
+			student.setFacultate(facultate);
+			studenti.add(student);
 		}
 		input.close();
 		return studenti;
