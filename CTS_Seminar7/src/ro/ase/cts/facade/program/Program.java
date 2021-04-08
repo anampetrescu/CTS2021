de2c9f72be6578e86d2ce45fa8f56d28ff1eeaa8
anampetrescu <anamaria.petrescu03@gmail.com>
2021-04-08 17:25:36 +0300

    Proxy+Facade+Adapter

---
+++ CTS_Seminar7/src/ro/ase/cts/facade/program/Program.java
@@ -0,0 +1,19 @@
+package ro.ase.cts.facade.program;
+
+import ro.ase.cts.facade.clase.Facade;
+import ro.ase.cts.facade.clase.Persoana;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Persoana persoana=new Persoana("2981220112222","Andreea");
+		if(Facade.esteEligibil(persoana)) {
+			System.out.println("Oferim credit");
+		}
+		else {
+			System.out.println("Nu oferim credit");
+		}
+
+	}
+
+}
