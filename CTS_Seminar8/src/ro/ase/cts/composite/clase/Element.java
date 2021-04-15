cd7f258f724deb0a35bf5f1e81dac7e7bc3d652a
anampetrescu <anamaria.petrescu03@gmail.com>
2021-04-15 17:11:34 +0300

    Composite+Decorator

---
+++ CTS_Seminar8/src/ro/ase/cts/composite/clase/Element.java
@@ -0,0 +1,36 @@
+package ro.ase.cts.composite.clase;
+
+public class Element implements ComponentaMeniu{
+	private String numeElement;
+
+	public Element(String numeElement) {
+		super();
+		this.numeElement = numeElement;
+	}
+
+	
+	@Override
+	public void adaugaNod(ComponentaMeniu componentaMeniu) {
+		throw new IllegalArgumentException("Nu ste implementata");
+		
+	}
+
+	@Override
+	public void stergeNod(ComponentaMeniu componentaMeniu) {
+		throw new IllegalArgumentException("Nu ste implementata");
+		
+	}
+
+	@Override
+	public ComponentaMeniu getNod(int index) {
+		throw new IllegalArgumentException("Nu ste implementata");
+
+	}
+
+	@Override
+	public void printeaza() {
+		System.out.println("Elementul " + numeElement);
+		
+	}
+
+}
