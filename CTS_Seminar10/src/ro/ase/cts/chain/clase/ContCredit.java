9664dfeb49e2db090b2d9c6edcb108e5e5dfe39e
anampetrescu <anamaria.petrescu03@gmail.com>
2021-05-06 16:47:02 +0300

    Chain of responsabilities

---
+++ CTS_Seminar10/src/ro/ase/cts/chain/clase/ContCredit.java
@@ -0,0 +1,25 @@
+package ro.ase.cts.chain.clase;
+
+public class ContCredit extends Cont{
+
+	public ContCredit(float sold) {
+		super(sold);
+	
+	}
+
+	@Override
+	public void plateste(float suma) {
+		if(super.getSold()>suma) {
+			System.out.println("Si platit cu contul de credit");
+			super.setSold(super.getSold()-suma);
+		}
+		else {
+			if(super.getSuccesor()==null) {
+				System.out.println("Nu se poate efectua plata");
+			}
+			else {
+				super.getSuccesor().plateste(suma);
+			}
+		}
+	}
+}
