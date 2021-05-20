8fe7e62d24650cf77bbf83830daee1a3d90515d4
anampetrescu <anamaria.petrescu03@gmail.com>
2021-05-20 17:02:43 +0300

    JUnit Tests

--- CTS_Seminar12/src/ro/ase/cts/tests/StudentTests.java
+++ CTS_Seminar12/src/ro/ase/cts/tests/StudentTests.java
@@ -51,5 +51,99 @@ public class StudentTests {
 		assertEquals(1,student.getNote().size());
 		
 	}
+	
+	@Test
+	public void testCalculeazaMedie() {
+		Student student=new Student();
+		int note1=10;
+		int note2=9;
+		int note3=5;
+		student.adaugaNota(note1);
+		student.adaugaNota(note2);
+		student.adaugaNota(note3);
+		float medie=(note1+note2+note3)/3.0f;
+		assertEquals(medie,student.calculeazaMedie(),0.01);
+		
+	}
+	
+	@Test
+	public void testCalculeazaMediePentruONota() {
+		Student student=new Student();
+		int note1=10;
+		student.adaugaNota(note1);
+		assertEquals(note1,student.calculeazaMedie(),0.01);
+		
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCalculeazaMediePentruZeroNote() {
+		Student student=new Student();
+		student.calculeazaMedie();
+		
+	}
+	
+	@Test
+	public void testAreRestanta() {
+		Student student=new Student();
+		int note1=6;
+		int note2=4;
+		int note3=2;
+		student.adaugaNota(note1);
+		student.adaugaNota(note2);
+		student.adaugaNota(note3);
+		assertTrue(student.areRestante());
+	}
+	
+	@Test
+	public void testNuAreRestanta() {
+		Student student=new Student();
+		int note1=8;
+		int note2=8;
+		int note3=5;
+		student.adaugaNota(note1);
+		student.adaugaNota(note2);
+		student.adaugaNota(note3);
+		assertFalse(student.areRestante());
+	}
+	
+	@Test
+	public void testGetNotaPozitieIncorecta() {
+		Student student=new Student();
+		int note1=6;
+		int note2=4;
+		int note3=2;
+		student.adaugaNota(note1);
+		student.adaugaNota(note2);
+		student.adaugaNota(note3);
+		//1
+		try {
+			//2
+			student.getNota(3);
+			//3
+			fail("metoda nu arunca exceptie");
+		}catch(IndexOutOfBoundsException e) {
+			//4
+		}
+		//5
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testAdaugaNotaIncorecta() {
+		Student student=new Student();
+		int nota=14;
+		student.adaugaNota(nota);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
