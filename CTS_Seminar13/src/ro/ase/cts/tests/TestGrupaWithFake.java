028f444599f96d8301fa2c4708446a894f285ad4
anampetrescu <anamaria.petrescu03@gmail.com>
2021-06-03 16:25:28 +0300

    JUnit Test MockUp-Dummy, Stub, Fake

---
+++ CTS_Seminar13/src/ro/ase/cts/tests/TestGrupaWithFake.java
@@ -0,0 +1,30 @@
+package ro.ase.cts.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ro.ase.cts.clase.Grupa;
+import ro.ase.cts.clase.dubluri.StudentFake;
+
+public class TestGrupaWithFake {
+
+	@Test
+	public void testPromovabilitateRight() {
+		Grupa grupa=new Grupa(1083);
+		for(int i=0;i<8;i++)
+		{
+			StudentFake student= new StudentFake();
+			student.setValoareAreRestante(false);
+			grupa.adaugaStudent(student);
+		}
+		for(int i=0;i<2;i++)
+		{
+			StudentFake student= new StudentFake();
+			student.setValoareAreRestante(true);
+			grupa.adaugaStudent(student);
+		}
+		assertEquals(0.8, grupa.getPromovabilitate(),0.01);
+	}
+
+}
